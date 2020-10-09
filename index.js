dc7e80a28a709b1ab1e2d0240234c22ebcd03f76
barunlab <barunlab@barunlabui-MacBook-Pro.local>
2020-10-09 16:02:01 +0900

    babel, nodemon -d install

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const express = require('express');
+import express from "express";
 const app = express();
 const PORT = 4000;
 
-function handleHome(req, res){
-    console.log(req);
-    res.send('홈 화면입니다.')
-}
-function handleProfile(req, res){
-    console.log(req);
-    res.send('프로필 화면입니다.');
-}
+const handleListening = () => console.log(`Example app listening on port ${PORT}!`);
+const handleHome = (req, res) => res.send('홈 화면입니다.');
+const handleProfile = (req, res) => res.send('프로필 화면입니다.');
 
 app.get('/', handleHome);
 app.get('/profile', handleProfile);
-
-function handleListening(){
-    console.log(`Example app listening on port ${PORT}!`);
-}
 
 app.listen(PORT, handleListening);
